b63e4dbcefb620a4658f91ebb691dbe788279631
Elan <elanadriano19@gmail.com>
2025-02-12 17:52:29 -0300

    Seventh commit

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ const display = document.querySelector('.display');
 const ton = document.getElementById('ton');
 const tlimpar = document.getElementById('tlimpar');
 const tigual = document.getElementById('tigual');
+const tcpy = document.getElementById('tcpy');
+const teste = document.getElementById('teste');
+const calc_aba = document.getElementById('calc_aba');
+const calc = document.getElementById('calc');
+const img_aba_calc = document.getElementById('img_aba_calc');
 
 let sinal = false;
 let decimal = false;
@@ -54,6 +59,20 @@ tlimpar.addEventListener('click', (evt) => {
 tigual.addEventListener('click', (evt) => {
   sinal = false;
   decimal = false;
-  const res = eval(display.innerHTML);
-  display.innerHTML = res;
+  display.innerHTML = eval(display.innerHTML);
+});
+
+tcpy.addEventListener('click', (evt) => {
+  // teste.select()
+  // teste.setSelectionRange(0,999999999)//mobile
+  // navigator.clipboard.writeText(teste.value)
+});
+
+calc_aba.addEventListener('click', (evt) => {
+  calc.classList.toggle('calc_exibir');
+  if (calc.classList.contains('calc_exibir')) {
+    img_aba_calc.setAttribute('src', 'src/img/seta_esquerda.svg');
+  } else {
+    img_aba_calc.setAttribute('src', 'src/img/seta_direita.svg');
+  }
 });
